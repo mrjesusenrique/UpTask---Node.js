5d8b5c57c91ac9b63becd066bbf6c9806d14ed3a
Jesús Casañas <jesusenrique9773@gmail.com>
2021-06-02 23:54:58 -0300

    Santizando las entradas en el nombre del Proyecto

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@
 const express = require('express');
 const router = express.Router();
 const proyectController = require('../controllers/proyectController');
+const { body } = require('express-validator/check');
 
 module.exports = function () {
     router.get('/', proyectController.proyectHome);
     router.get('/nuevo-proyecto', proyectController.proyectNuevoProyecto);
-    router.post('/nuevo-proyecto', proyectController.proyectGuardarNuevoProyecto);
+    router.post('/nuevo-proyecto',
+        body('nombre').not().isEmpty().trim().escape(),
+        proyectController.proyectGuardarNuevoProyecto);
     return router;
 };
 
